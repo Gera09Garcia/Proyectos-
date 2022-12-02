8fb4c5b041edb31707611a1ea6b8a6b3fb3f7ced
trejo09g <garcia.adansrt@gmail.com>
2022-12-01 21:51:21 -0700

    Modificacion 1

---
+++ src/Datos.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author gerag
+ */
+public class Datos {
+     Informacion info = new Informacion("Adan Trejo","ING.Sistemas","Segundo Semestre");
+        // encapsulamiento
+        String NumCon = "\n" + info.getNombre()+ "\n" +info.getMateria()+ "\n" +info.getSemestre();
+      //  System.out.println("\n Datos generales alumno(a):" + NumCon);
+       // System.out.println("Hola");
+}
---
+++ src/Informacion.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author gerag
+ */
+class Informacion {
+
+    Informacion(String adan_Trejo, String ingSistemas, String segundo_Semestre) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    String getNombre() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    String getMateria() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    String getSemestre() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
